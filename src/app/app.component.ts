4133526b2377d089411cd386589f6611e4d7cdd3
milostomsik1 <milostomsik1@gmail.com>
2017-10-01 14:20:59 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  constructor(private http: Http) {}
+
+  // --- sets property as observable, maps to json so it matches <object[]> type and becomes iterable, by default type is Response
+  users$: Observable<object[]> = this.http.get('https://api.github.com/users').map(res => res.json());
+
+  ngOnInit() {
+    // --- can later subscribe to user$
+    this.users$.subscribe(users => {
+      console.log(users);
+    });
+  }
 }
